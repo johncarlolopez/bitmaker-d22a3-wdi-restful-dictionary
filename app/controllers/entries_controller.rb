0a50bf8dd9572fb3ea8c967a9b81ee820fbf8cca
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-10 14:48:03 -0500

    create index and show pages

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,6 @@
+class EntriesController < ApplicationController
+  def index
+    # here we'll define some @instance_variables to store data from the database for the views to use
+    render :index
+  end
+end
