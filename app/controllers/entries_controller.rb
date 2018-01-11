668f6485979f7aefa840531e06a72675f266f51a
John Carlo Lopez <condottiere.jc@gmail.com>
2018-01-11 16:20:28 -0500

    add CRUD functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,43 @@
 class EntriesController < ApplicationController
   def index
-    # here we'll define some @instance_variables to store data from the database for the views to use
+    @entries = Entry.all
     render :index
   end
+  def show
+    @entry = Entry.find(params[:id])
+  end
   def create
-    redirect_to entries_url
+    @entry = Entry.new(entry_params)
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to new_entry_path
+    end
+  end
+  def new
+    @entry = Entry.new
   end
   def edit
+    @entry = Entry.find(params[:id])
     render :edit
   end
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+    @entry.update(entry_params)
+
+    if @entry.save
+      redirect_to entry_path
+    else
+      redirect_to entry_path(params[:id])
+    end
   end
   def destroy
-    redirect_to index_url
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
+  end
+  def entry_params
+    params.require(:entry).permit(:word, :language, :definition)
   end
 end
